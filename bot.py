19b981b1673ad3230e501fbecf1a522940a22da8
Ilya <balashovia@bk.ru>
2021-03-26 14:17:54 +0300

    Убрал конфиг с токеном и венв в .gitignore

--- bot.py
+++ bot.py
@@ -1,16 +1,18 @@
 import config
-import exchange
+# import exchange
 import telebot
-import datetime
+# import datetime
 import pytz
-import json
-import traceback
+# import json
+# import traceback
+
 
 P_TIMEZONE = pytz.timezone(config.TIMEZONE)
 TIMEZONE_COMMON_NAME = config.TIMEZONE_COMMON_NAME
 
 bot = telebot.TeleBot(config.TOKEN)
-bot.polling(none_stop=True)
+
+
 
 @bot.message_handler(commands=['start'])
 def start_command(message):
@@ -21,40 +23,36 @@ def start_command(message):
         'Чтобы запросить помощь - нажми /help.'
   )
 
-@bot.message_handler(commands=['help'])
-def help_command(message):
-    keyboard = telebot.types.InlineKeyboardMarkup()
-    keyboard.add(
-        telebot.types.InlineKeyboardButton(
-            'Message the developer', url='telegram.me/balashovia'
-  )
-    )
-    bot.send_message(
-        message.chat.id,
-        '1) To receive a list of available currencies press /exchange.\n' +
-        '2) Click on the currency you are interested in.\n' +
-        '3) You will receive a message containing information regarding the source and the target currencies, ' +
-        'buying rates and selling rates.\n' +
-        '4) Click “Update” to receive the current information regarding the request. ' +
-        'The bot will also show the difference between the previous and the current exchange rates.\n' +
-        '5) The bot supports inline. Type @<botusername> in any chat and the first letters of a currency.',
-        reply_markup=keyboard
-    )
 
+# @bot.message_handler(commands=['help'])
+# def help_command(message):
+#     keyboard = telebot.types.InlineKeyboardMarkup()
+#     keyboard.add(
+#         telebot.types.InlineKeyboardButton(
+#             'Message the developer', url='telegram.me/balashovia'
+#   )
+#     )
+#     bot.send_message(
+#         message.chat.id,
+#         '1) Чтобы увидеть список доступных валют, нажмите /exchange.\n' +
+#         '2) Нажмите на интересующую Вас валюту.\n' +
+#         '3) Вы получите информацию по продаже и покупке выбранной валюты по курсу ЦБ.\n' +
+#         '4) Нажмите “Update”, чтобы обновить текущую информацию по курсу.\n' +
+#         '5) Поддержка Бота всегда на связи. Напишите telegram.me/balashovia по любым вопросам и предложениям',
+#         reply_markup=keyboard
+#     )
+#
+#
+# @bot.message_handler(commands=['exchange'])
+# def exchange_command(message):
+#     keyboard = telebot.types.InlineKeyboardMarkup(row_width=4)
+#     for val in exchange.load_exchange()['Valute']:
+#         btn = telebot.types.InlineKeyboardButton(f'{val}', callback_data=f'btn-{val}')
+#         keyboard.add(btn)
+#     bot.send_message(
+#         message.chat.id,
+#         'Выберите валюту:',
+#         reply_markup=keyboard
+#     )
 
-@bot.message_handler(commands=['exchange'])
-def exchange_command(message):
-    keyboard = telebot.types.InlineKeyboardMarkup()
-    keyboard.row(
-        telebot.types.InlineKeyboardButton('USD', callback_data='get-USD')
-    )
-    keyboard.row(
-        telebot.types.InlineKeyboardButton('EUR', callback_data='get-EUR'),
-        telebot.types.InlineKeyboardButton('RUR', callback_data='get-RUR')
-    )
-
-    bot.send_message(
-        message.chat.id,
-        'Click on the currency of choice:',
-        reply_markup=keyboard
-    )
+bot.polling(none_stop=True)
